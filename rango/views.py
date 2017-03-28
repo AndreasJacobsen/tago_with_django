e49b7bd011b6658be12953803af361db3787c2a8
vimes90 <andreas1300@gmail.com>
2017-03-28 11:44:37 +0200

    brushan

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hello world!")
+    return HttpResponse("Rango says hello world! <br> <a href='/rango/about'>About</a>")
 
 def about(request):
     return HttpResponse("Rango says: This is the about page!")
